3636a42b007bcd80ef7f1e6f6f0ed792f597a8da
Oleksiy Dubovyk <oleksiy_dubovyk@epam.com>
2018-05-11 22:35:02 +0300

    iterators

--- app/7.iterators.js
+++ app/7.iterators.js
@@ -13,10 +13,29 @@ const colors = {
   pink: '#e07'
 };
 
+const keyValueIterable = (target) => {
+  const entries = Object.entries(target);
+  
+  return {
+    [Symbol.iterator]() {
+      return {
+        next() {
+          const done = !entries.length;
+          const value = done ? undefined : entries.shift();
+          return { value, done };
+        }
+      }
+    }
+  }
+}
+
+
 const itColors = keyValueIterable(colors);
 for (const [, color] of itColors) {
   console.log(color);
 }
+
+
 
 // Task 2
 // RU: Написать функцию take(sequence, amount), которая из бесконечного итерируемого объекта random
@@ -32,8 +51,19 @@ const random = {
   })
 };
 
+const take = (sequence, amount) => {
+  const iterable = sequence[Symbol.iterator]();
+  const arr = [];
+  for (let i = 0; i < amount; i++) {
+    arr.push(iterable.next().value);
+  }
+  return arr;
+}
+
 const a = [...take(random, 3)];
 console.log(a);
+
+
 
 // Task 3
 // RU: Написать итерируемый итератор, который возвращает числа Фибоначи
@@ -41,10 +71,33 @@ console.log(a);
 // EN: Create iterable iterator, which produces Fibonacci numbers
 //     Implement method return, which allows you to stop iterator using for-of + break
 
+const Fib = {
+  [Symbol.iterator]() {
+    let a1 = 1;
+    let a2 = 1;
+    let cur;
+
+    return {
+      next() {
+        cur = a1;
+        a1 = a2;
+        a2 = cur + a1;
+
+        return {
+          value: cur,
+          done: false,
+        }
+      }
+    }
+  }
+};
+
 for (let v of Fib) {
   console.log(v);
   if (v > 50) break;
 }
+
+
 
 // Task 4
 // RU: Написать итератор для чисел, который позволит получать массивы последовательных целых элементов.
@@ -52,5 +105,19 @@ for (let v of Fib) {
 // EN: Create iterator for numbers, which allows you to get arrays of sequential integers.
 //     Example, [...-3] => [0, -1, -2, -3], [...3] => [0, 1, 2, 3]
 
+Number.prototype[Symbol.iterator] = function() {
+  const initNumber = this;
+  let curNumber = 0;
+
+  return {
+    next() {
+      const done = Math.abs(curNumber) > Math.abs(initNumber);
+      const value = done ? undefined : (initNumber > 0 ? curNumber++ : curNumber--);
+      return { value, done };
+    }
+  }
+}
+
 console.log([...-5]);
 console.log([...5]);
+
