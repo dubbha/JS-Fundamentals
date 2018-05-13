83af6450699449572f2c28ef662e2ab41c8719f1
Oleksiy Dubovyk <oleksiy_dubovyk@epam.com>
2018-05-13 21:40:32 +0300

    date, iterators

--- app/3.2.date.js
+++ app/3.2.date.js
@@ -1,5 +1,12 @@
 console.log('Object Date');
 
+/*
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+http://learn.javascript.ru/datetime
+
+*/
+
 // Task 1
 // RU: Создать текущую дату и вывести ее в формате dd.mm.yyyy и dd Month yyyy
 // EN: Create current date and display it in the console according to the format
@@ -53,49 +60,153 @@ console.log('Object Date');
 // Task 4
 // RU: Подсчитать время суммирования чисел от 1 до 1000.
 // EN: Calculate the time of summing numbers from 1 to 1000.
-{
 
-  const start = Date.now();       // unix epoch milliseconds
+// Using Date.now()
+{
+  let forLoopTime;
+  let arrayFillReduceTime;
+  let arrayFillMapReduceTime;
+
+  let start;
+  
+  start = Date.now();       // unix epoch milliseconds, same as +new Date()
 
   let sum = 0;
   for(let i = 1; i <= 1000000; i++) {
       sum += i;
   }
 
-  console.log(`for loop used ${Date.now() - start} msecs`);      // ~60 msec
-  console.log(`sum: ${sum}`);     // 500000500000
-
+  forLoopTime = Date.now() - start;
   
-  const start2 = Date.now();
+
+  start = Date.now();
 
   const sum2 = Array(1000000).fill().reduce((acc, e, i) => acc + i + 1, 0);
 
-  console.log(`Array(n).fill().reduce() used ${Date.now() - start2} msecs`);  // ~90 msec
-  console.log(`sum: ${sum2}`);     // 500000500000
+  arrayFillReduceTime = Date.now() - start;
 
 
-  const start3 = Date.now();
+  start = Date.now();
 
   const sum3 = Array(1000000).fill().map((e, i) => +i + 1).reduce((acc, e) => acc + e, 0);
 
-  console.log(`Array().fill().map().reduce() used ${Date.now() - start3} msecs`);    // 300 msec
-  console.log(`sum: ${sum3}`);     // 500000500000
+  arrayFillMapReduceTime = Date.now() - start;
+
+
+  console.log(`for loop took ${forLoopTime} ms`);
+  console.log(`Array().fill().reduce() used ${arrayFillReduceTime} ms`);
+  console.log(`Array().fill().map().reduce() used ${arrayFillMapReduceTime} ms`);
+
+}
+
+// Using performance.now()
+// exists in browsers, much more accurate due to higher precision [preferred]
+{
+  let start;
+  
+  start = performance.now();       // unix epoch milliseconds, same as +new Date()
+
+  let sum = 0;
+  for(let i = 1; i <= 1000000; i++) {
+      sum += i;
+  }
+
+  forLoopTime = performance.now() - start;
+
+  
+  start = performance.now();
+
+  const sum2 = Array(1000000).fill().reduce((acc, e, i) => acc + i + 1, 0);
+
+  arrayFillReduceTime = performance.now() - start;
+
+
+  start = performance.now();
+
+  const sum3 = Array(1000000).fill().map((e, i) => +i + 1).reduce((acc, e) => acc + e, 0);
+
+  arrayFillMapReduceTime = performance.now() - start;
+
+
+  console.log(`for loop took ${forLoopTime} ms`);
+  console.log(`Array(n).fill().reduce() used ${arrayFillReduceTime} ms`);
+  console.log(`Array().fill().map().reduce() used ${arrayFillMapReduceTime} ms`);
+}
+
+// Benchmarking
+// A better benchmarking is to run the functions many times,
+// switching between them to avoid current CPU load from affecting the performance
+{
+  const forLoop = (num) => {
+    let sum = 0;
+    for(let i = 1; i <= num; i++) {
+        sum += i;
+    }
+    return sum;
+  }
+
+  const arrayFillReduce = (num) =>
+    Array(num).fill().reduce((acc, e, i) => acc + i + 1, 0);
+
+  const arrayFillMapReduce = (num) =>
+    Array(num).fill().map((e, i) => +i + 1).reduce((acc, e) => acc + e, 0);
+
+  const times = 15;                   // time to run the benchmark, the higher the better
+  const num = 1000000;                // each function should sum up to this number
+
+  let forLoopTotalTime = 0;           // total time all the runs took
+  let arrayFillReduceTotalTime = 0;
+  let arrayFillMapReduceTotalTime = 0;
+
+  let start;                            // var used to store starting times
+  for (let i = 0; i < times; i++) {
+    start = performance.now();
+    forLoop(num);
+    forLoopTotalTime += performance.now() - start;
+
+    start = performance.now();
+    arrayFillReduce(num);
+    arrayFillReduceTotalTime += performance.now() - start;
+
+    start = performance.now();
+    arrayFillMapReduce(num);
+    arrayFillMapReduceTotalTime += performance.now() - start;
+  }
+
+  const forLoopAvgTime = forLoopTotalTime / times;
+  const arrayFillReduceAvgTime = arrayFillReduceTotalTime / times;
+  const arrayFillMapReduceAvgTime = arrayFillMapReduceTotalTime / times;
+
+  console.log(`forLoop on average took ${forLoopAvgTime} msecs`);
+  console.log(`arrayFillReduce on average took ${arrayFillReduceAvgTime} msecs`);
+  console.log(`arrayFillMapReduce on average took ${arrayFillMapReduceAvgTime} msecs`);
 
 }
 
 // Task 5
 // RU: Подсчитать количество дней с текущей даты до Нового года.
 // EN: Calculate the number of days from the current date to the New Year.
+
+// Pass two arguments to Date()
 {
     const now = new Date();
-    const ny = new Date(now.getFullYear() + 1, 0);      // must pass at least two params, otherwise a single param will be considered epoch msecs
+    const ny = new Date(now.getFullYear() + 1, 0);      // Must pass at least two params, otherwise
+                                                        // single param will be considered epoch msecs.
+                                                        // Or you could explicitly convert it to string:
+                                                        // new Date(`${now.getFullYear()}`);
 
-    console.log(ny);
-
-    console.log(now.getTime());
-    console.log('ny.getTime()', ny.getTime());
     const msecsLeft = ny.getTime() - now.getTime();
-    console.log(msecsLeft);
     
     console.log(`Days left till New Year: ${Math.floor(msecsLeft / (1000 * 60 * 60* 24))}`)
+}
+
+// Explicitly convert a single argument to string
+// If a single argument is a string, Date() uses Date.parse to parse it
+{
+  const now = new Date();
+  const ny = new Date(`${now.getFullYear() + 1}`);     // explicitly convert argument to string
+
+  const msecsLeft = ny.getTime() - now.getTime();
+  
+  console.log(`Days left till New Year: ${Math.floor(msecsLeft / (1000 * 60 * 60* 24))}`)
 }
--- app/7.iterators.js
+++ app/7.iterators.js
@@ -7,32 +7,33 @@
 //     which takes an objects and returns iterable object.
 //     Iterable object allows you to get key/value pairs.
 //     Display in a console all colors from the object colors.
-const colors = {
-  green: '#0e0',
-  orange: '#f50',
-  pink: '#e07'
-};
-
-const keyValueIterable = (target) => {
-  const entries = Object.entries(target);
+{
+  const colors = {
+    green: '#0e0',
+    orange: '#f50',
+    pink: '#e07'
+  };
   
-  return {
-    [Symbol.iterator]() {
-      return {
-        next() {
-          const done = !entries.length;
-          const value = done ? undefined : entries.shift();
-          return { value, done };
+  const keyValueIterable = (target) => {
+    const entries = Object.entries(target);
+    
+    return {
+      [Symbol.iterator]() {
+        return {
+          next() {
+            const done = !entries.length;
+            const value = done ? undefined : entries.shift();
+            return { value, done };
+          }
         }
       }
     }
   }
-}
-
-
-const itColors = keyValueIterable(colors);
-for (const [, color] of itColors) {
-  console.log(color);
+  
+  const itColors = keyValueIterable(colors);
+  for (const [, color] of itColors) {
+    console.log(color);
+  }  
 }
 
 
@@ -42,27 +43,27 @@ for (const [, color] of itColors) {
 //     вернет указаное количество элементов.
 // EN: Create a function take(sequence, amount), which returns a specified amount of numbers
 //     from iterable object random
-
-const random = {
-  [Symbol.iterator]: () => ({
-    next: () => ({
-      value: Math.random()
+{
+  const random = {
+    [Symbol.iterator]: () => ({
+      next: () => ({
+        value: Math.random()
+      })
     })
-  })
-};
-
-const take = (sequence, amount) => {
-  const iterable = sequence[Symbol.iterator]();
-  const arr = [];
-  for (let i = 0; i < amount; i++) {
-    arr.push(iterable.next().value);
+  };
+  
+  const take = (sequence, amount) => {
+    const iterable = sequence[Symbol.iterator]();
+    const arr = [];
+    for (let i = 0; i < amount; i++) {
+      arr.push(iterable.next().value);
+    }
+    return arr;
   }
-  return arr;
+  
+  const subsequence = [...take(random, 3)];
+  console.log(subsequence);  
 }
-
-const subsequence = [...take(random, 3)];
-console.log(subsequence);
-
 
 
 // Task 3
@@ -73,72 +74,46 @@ console.log(subsequence);
 
 // http://exploringjs.com/es6/ch_iteration.html#_iterators-that-are-iterable
 // http://jsrocks.org/2015/09/javascript-iterables-and-iterators
-
-const Fib = {
-  a1: 1,
-  a2: 1,
-
-  next() {              // iterator - The Iterator interface requires the implementation of a next() method.
-    cur = this.a1;
-    this.a1 = this.a2;
-    this.a2 = cur + this.a1;
-
-    return {
-      value: cur,
-      done: false,
+{
+  const Fib = {
+    a1: 1,
+    a2: 1,
+  
+    next() {              // iterator - The Iterator interface requires the implementation of a next() method.
+      cur = this.a1;
+      this.a1 = this.a2;
+      this.a2 = cur + this.a1;
+  
+      return {
+        value: cur,
+        done: false,
+      }
+    },
+  
+    [Symbol.iterator]() { // iterable - The Iterable interface requires the implementation of a [Symbol.iterator]() method;
+      return this;
+    },
+  
+    return() {  // http://exploringjs.com/es6/ch_iteration.html#_optional-iterator-methods-return-and-throw
+      // you could clean up some external resources here
+      return { done: true };
     }
-  },
-
-  [Symbol.iterator]() { // iterable - The Iterable interface requires the implementation of a [Symbol.iterator]() method;
-    return this;
-  },
-
-  return() {  // http://exploringjs.com/es6/ch_iteration.html#_optional-iterator-methods-return-and-throw
-    // you could clean up some external resources here
-    return { done: true };
+  };
+  
+  Fib[Symbol.iterator]() === Fib;   // true -- an iterable iterator pattern
+  
+  for (let v of Fib) {      // 3 ... 8
+    console.log(v);
+    if (v > 5) break;      // break method resets the counter
   }
-};
-
-Fib[Symbol.iterator]() === Fib;   // true -- an iterable iterator pattern
-
-for (let v of Fib) {      // 3 ... 8
-  console.log(v);
-  if (v > 5) break;      // break method resets the counter
-}
-
-console.log('because iterator is iterable, you can continue an iteration in another loop');
-
-for (let v of Fib) {      // 13 ... 55
-  console.log(v);
-  if (v > 50) break;      // break method resets the counter
-}
-
-// Another school way to do the same (using internal [Symbol.iterator]() and )
-const Fib = {
-  a1: 1,
-  a2: 1,
-
-  next() {              // iterator - The Iterator interface requires the implementation of a next method.
-    cur = this.a1;
-    this.a1 = this.a2;
-    this.a2 = cur + this.a1;
-
-    return {
-      value: cur,
-      done: false,
-    }
-  },
-
-  [Symbol.iterator]() { // iterable - The Iterable interface requires the implementation of a [Symbol.iterator] method;
-    return this;
-  },
-
-  return() {  // http://exploringjs.com/es6/ch_iteration.html#_optional-iterator-methods-return-and-throw
-    // you could clean up some external resources here
-    return { done: true };
+  
+  console.log('because iterator is iterable, you can continue an iteration in another loop');
+  
+  for (let v of Fib) {      // 13 ... 55
+    console.log(v);
+    if (v > 50) break;      // break method resets the counter
   }
-};
-
+}
 
 
 // Task 4
@@ -146,19 +121,20 @@ const Fib = {
 //     Например, [...-3] => [0, -1, -2, -3], [...3] => [0, 1, 2, 3]
 // EN: Create iterator for numbers, which allows you to get arrays of sequential integers.
 //     Example, [...-3] => [0, -1, -2, -3], [...3] => [0, 1, 2, 3]
-
-Number.prototype[Symbol.iterator] = function() {
-  const initNumber = this;
-  let curNumber = 0;
-
-  return {
-    next() {
-      const done = Math.abs(curNumber) > Math.abs(initNumber);
-      const value = done ? undefined : (initNumber > 0 ? curNumber++ : curNumber--);
-      return { value, done };
+{
+  Number.prototype[Symbol.iterator] = function() {
+    const initNumber = this;
+    let curNumber = 0;
+  
+    return {
+      next() {
+        const done = Math.abs(curNumber) > Math.abs(initNumber);
+        const value = done ? undefined : (initNumber > 0 ? curNumber++ : curNumber--);
+        return { value, done };
+      }
     }
   }
+  
+  console.log([...-5]);
+  console.log([...5]);  
 }
-
-console.log([...-5]);
-console.log([...5]);
